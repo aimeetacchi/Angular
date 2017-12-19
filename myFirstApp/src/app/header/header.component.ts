5bfe2e178e5ecacfbd4aa14fe3abd70d2468e45a
darkxangel84 <aimeetacchi@hotmail.co.uk>
2017-12-19 16:28:46 +0000

    databinding

--- myFirstApp/src/app/header/header.component.ts
+++ myFirstApp/src/app/header/header.component.ts
@@ -1,13 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {
-
+export class HeaderComponent implements OnInit {
+	@Input() testing: string;
 	logo = './assets/images/logo.png';
-  
+  	ngOnInit(){
+  		console.log(this.testing);
+  	}
 
 }
