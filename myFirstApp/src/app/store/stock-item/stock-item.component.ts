b5c7c6a6acb270c728d6ec49d2d0bc9f2fb62120
darkxangel84 <aimeetacchi@hotmail.co.uk>
2017-12-20 16:40:59 +0000

    add up items bought

--- myFirstApp/src/app/store/stock-item/stock-item.component.ts
+++ myFirstApp/src/app/store/stock-item/stock-item.component.ts
@@ -9,31 +9,45 @@ export class StockItemComponent implements OnInit {
 	title = "Our Stock";
 
 	items: Stock[] = [
-	{title: 'Shoes',
+	{id: 1,
+	title: 'Shoes',
 	quantity: 5,
+	price: 15,
 	description: 'These shoes are one of a kind in size 6, they are blue',
 	sold: false},
-	{title: 'Cat Tshirt',
+	{id: 2,
+	title: 'Cat Tshirt',
 	quantity: 10,
+	price: 5,
 	description: 'Womens cat tshirt for all them cat lovers',
 	sold: false},
-	{title: 'Pokemon Tshirt',
+	{id: 3,
+	title: 'Pokemon Tshirt',
 	quantity: 4,
+	price: 4,
 	description: 'Adults Pikachu Tshirt for all those fans',
 	sold: false},
-	{title: 'Totoro Tshirt',
+	{id: 4,
+	title: 'Totoro Tshirt',
 	quantity: 4,
+	price: 4,
 	description: 'Adults Totoro Tshirt for all those fans',
 	sold: false},
-	{title: 'Sonic Tshirt',
-	quantity: 4,
+	{id: 5,
+	title: 'Sonic Tshirt',
+	quantity: 0,
+	price: 4,
 	description: 'Adults Sonic Tshirt for all those fans',
-	sold: false},
-	{title: 'Sonic Socks',
+	sold: true},
+	{id: 6,
+	title: 'Sonic Socks',
 	quantity: 4,
+	price: 2,
 	description: 'Adults Sonic Socks for all those fans',
 	sold: false}]
-  
+  	
+	sold: any[] = [];
+	total: number = 0;
   constructor(
   	){}
 
@@ -41,11 +55,22 @@ export class StockItemComponent implements OnInit {
   	console.log(this.items);
   }
 
+
+
+  public buyItem(item){
+  		//console.log(item);
+  		this.total += item.price;
+  		console.log(this.total);
+
+  	}
+
 }
 
 interface Stock {
+	id: number;
 	title: string;
 	quantity: number;
+	price: number;
 	description: string;
 	sold: boolean;
 }
